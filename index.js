7772fe370ed419336aff5e3d04d4c9c96b3edc34
Matheus Catossi <mcatossi@br.ibm.com>
2019-06-13 20:23:53 -0300

    add random

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ app.use(cors("*"));
 const connFactory = require("./connection/connFactory.js");
 
 let params = {
-    "username": "",
-    "host": "",
+    "username": "96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix",
+    "host": "96ba32ad-e17d-494f-a93e-72240b1e0b16-bluemix.cloudant.com",
     "dbname": "talk-alphaville",
-    "password": ""
+    "password": "e373c010bcb53c3ea89a59f7fa2642789e7bfe3128bab1c3e2762b713ab04641"
 };
 
 app.get('/api/test', function (req, res) {
+    console.log("GET /api/test");
+    
+    
+    min = Math.ceil(0);
+    max = Math.floor(20);
+
     res.send({
-        result: "teste"
+        result: "MEETUP Inova!",
+        random: Math.floor(Math.random() * (max - min + 1)) + min
     })
 });
 
